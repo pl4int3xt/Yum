819290adb79fdc562edd8b4929f755941e53e27b
BENJAH <benjahjp@gmail.com>
2023-01-23 20:18:02 +0300

    first commit

--- app/src/main/java/com/example/kernel/MainActivity.kt
+++ app/src/main/java/com/example/kernel/MainActivity.kt
@@ -1,22 +1,54 @@
 package com.example.kernel
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import com.example.kernel.ui.theme.KernelTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView)
+
+        windowInsetsController?.isAppearanceLightNavigationBars = true
+
         setContent {
             KernelTheme {
                 // A surface container using the 'background' color from the theme
@@ -24,25 +56,41 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                colors = TopAppBarDefaults.smallTopAppBarColors(
+                                    containerColor = androidx.compose.ui.graphics.Color.Transparent,
+                                    titleContentColor = androidx.compose.ui.graphics.Color.White,
+                                    navigationIconContentColor = androidx.compose.ui.graphics.Color.White
+                                ),
+                                navigationIcon = {
+                                    IconButton(
+                                        modifier = Modifier
+                                            .background(
+                                                color = androidx.compose.ui.graphics.Color.Transparent,
+                                                shape = CircleShape
+                                            )
+                                            .padding(5.dp)
+                                        ,
+                                        onClick = { /*TODO*/ }) {
+                                        Icons.Default.ArrowBack
+                                    }
+                                },
+                                title = {
+                                    Text(text = "House")
+                                }
+                            )
+                        }
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            Image(painter = painterResource(id = R.mipmap.images), contentDescription = "image")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KernelTheme {
-        Greeting("Android")
     }
 }
