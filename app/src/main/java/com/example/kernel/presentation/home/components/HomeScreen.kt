9a19be5cdda9c4ddb0c13ba9c90a4617a3cfb795
BENJAH <benjahjp@gmail.com>
2023-01-24 13:26:07 +0300

    fix: categorycard and MainTopAppBar

--- app/src/main/java/com/example/kernel/presentation/home/components/HomeScreen.kt
+++ app/src/main/java/com/example/kernel/presentation/home/components/HomeScreen.kt
@@ -2,8 +2,10 @@ package com.example.kernel.presentation.home.components
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -24,8 +26,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.kernel.presentation.home.HomeScreenViewModel
 import com.example.kernel.presentation.shared.MainTopAppBar
@@ -68,13 +72,25 @@ fun HomeScreen(
         ) {
             LazyColumn(){
                 item {
+                    Column(
+                        modifier = Modifier.height(200.dp)
+                    ) {
 
+                    }
                 }
                 item {
-                    Text(text = "Find Best Recipe\nfor cooking")
+                    Text(
+                        text = "Find Best Recipe\nfor cooking",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 25.sp
+                    )
                 }
                 item {
-                    Text(text = "Categories")
+                    Text(
+                        text = "Categories",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
                 }
                 items(state.categories.size){ i ->
                     CategoryCard(
