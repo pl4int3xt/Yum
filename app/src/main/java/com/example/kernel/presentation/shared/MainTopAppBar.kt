22041b7580a986edf7b8031d6ea03fb73d5bbdb2
BENJAH <benjahjp@gmail.com>
2023-01-24 18:13:12 +0300

    fix:

--- app/src/main/java/com/example/kernel/presentation/shared/MainTopAppBar.kt
+++ app/src/main/java/com/example/kernel/presentation/shared/MainTopAppBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,6 +30,7 @@ fun MainTopAppBar(
     onClickAction: () -> Unit
 ) {
     TopAppBar(
+        scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(),
         modifier = Modifier.padding(5.dp),
         colors = TopAppBarDefaults.smallTopAppBarColors(
             containerColor = Color.Transparent,
