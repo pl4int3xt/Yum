0d984c44d8b578f8ae412507e2a27ff362bdd045
BENJAH <benjahjp@gmail.com>
2023-02-08 15:06:18 +0300

    fix

--- app/src/main/java/com/example/kernel/data/remote/dto/AreaDto.kt
+++ app/src/main/java/com/example/kernel/data/remote/dto/AreaDto.kt
@@ -1,5 +1,8 @@
 package com.example.kernel.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class AreaDto(
     val meals: List<MealX>
 )
--- app/src/main/java/com/example/kernel/data/remote/dto/MealX.kt
+++ app/src/main/java/com/example/kernel/data/remote/dto/MealX.kt
@@ -1,7 +1,9 @@
 package com.example.kernel.data.remote.dto
 
 import com.example.kernel.domain.model.AreaModel
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class MealX(
     val strArea: String
 )
