ac34400568994b1e4af407cbe7983f76cf4e209f
BENJAH <benjahjp@gmail.com>
2023-02-08 16:16:28 +0300

    fix

--- app/src/main/java/com/example/kernel/MainActivity.kt
+++ app/src/main/java/com/example/kernel/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import com.example.kernel.presentation.navigation.MainNavGraph
@@ -16,6 +17,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             KernelTheme {
